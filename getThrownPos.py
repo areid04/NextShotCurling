9eff2ab806cbec5cc4a1fa1f9e88dd16f7352a13
Alexander Reid <114508072+areid04@users.noreply.github.com>
2023-06-20 23:04:09 -0400

    Update getThrownPos.py
    Updated logic to prevent 'nightmare thrown stone situations' (see images)

--- getThrownPos.py
+++ getThrownPos.py
@@ -8,18 +8,24 @@ import imutils
 def getThrownCord(imgp):
     cx = 0;
     cy = 0;
-    reta, thresha = cv2.threshold(imgp, 0, 4, 0)
+    reta, thresha = cv2.threshold(imgp, 0, 10, 0)
     contoursa, hierachya = cv2.findContours(thresha, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contour_list = []
     for contour in contoursa:
         approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
         area = cv2.contourArea(contour)
-        if ((len(approx) > 8) & (area > 20) & (area < 250)):
+        if ((len(approx) > 1) & (area > 20) & (area < 150)):
             contour_list.append(contour)
             M = cv2.moments(contour)
+
             if M['m00'] != 0:
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
+                print('recognized at' + str(cx) + ' ' + str(cy))
+                cv2.imshow('roi', imgp)
+                cv2.waitKey(0)
+                cv2.drawContours(imgp, contour, -1, (0, 255, 0), 5)
+                break;
     sol = []
     sol.append(cx)
     sol.append(cy)
